05e476dd019867da8162772c1af0a5828b4017bb
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2021-09-18 13:56:31 +0300

    Update Point1.jl
    moving_around не оптимизирована. tuda_suda нужно удалить.

--- seminar2/Point1.jl
+++ seminar2/Point1.jl
@@ -1,69 +1,90 @@
-function movements_in_angle!(r::Robot, side::HorizonSide, massive::Vector{<:Integer})::Nothing
-    while isborder!(r,side) == false
-        move!(r, side)
-        massive[HorizonSide(side)+1] +=1
-    end
-end
-
+# PATH это массив, который хранит путь из левой нижней клетки в начальную.
 PATH = []
-path = [0,0]
+# path(x,y) это массив, который хранит начальные коррдинаты робота.  
+path = [0, 0]
 
-function pathXY(r)::Vector{<:Integer}
-    for i in 0:3
-        while 
-end
-
-function movements!(r::Robot)::Vector{<:Integer}
+# movements_in_angle двигает робота из начальной клетки в левую нижнюю.
+function moving_in_angle!(r)::Vector{<:Integer}
     while not(isborder(r,Sud) && isborder(r,West)) 
-        for side in (Sud, West)
+        for side in (West, Sud)
             steps = 0
             while not(isborder(r,side))
                 move!(r,side)
                 steps += 1
-            end
-            push!(PATH,steps) 
+            push!(PATH, steps)
         end
     end
-    return revers!(PATH)
+    return reverse!(PATH)
 end
 
-function pathXY(massive::Vector{<:Integer})
-
+# pathXY складывает все соответсвтеные координаты пути, записанного в PATH и получает начальные координаты робота.
+function pathXY()::Vector{<:Integer}
+    for i in lenght(PATH)
+        path[2 - (i%2)] += PATH[i]*(i%2==0) + PATH[i]*(i%2!=0) 
+    end
 end
 
-function back_in_start!(r::Robot)::Nothing
+# moving_back_to_start двигает робота из нижней левой клетки в начальную по обратному пути из массива PATH
+function moving_back_to_start(r)::Nothing
     for i in lenght(PATH)
         for a in PATH[i]
-            move!(r, HorizonSide(i%2)) 
+            move!(r, HorizonSide((i+2)%4))
         end
     end
 end
 
-function go_by_border!(r::Robot, side::HorizonSide, massive::Vector{<:Integer})::Nothing
-    for steps in massive[abs( (HorizonSide(side)+3) - (HorisinSide(side)+1) )]
+# tuda_suda делает туда-сюда
+function tuda_suda(side)::Integer
+    steps = 0
+    while not(isborder(side))
         move!(r,side)
-        massive[HorisinSide(side)+3] += 1
-        putmarker!(r)
-        while (isborder(side) == false)
-            move!(side)
-            massive[HorisinSide(side)+3] += 1
-        end
-    end  
+        steps += 1
+    while not(ismarked(HorizonSide((Int(side)+2)%4))
+        move!(r,HorizonSide((Int(side)+2)%4))
+    return steps
 end
 
-function master_function(r::Robot)
-    
-    movements!(r)
+# moving_around двигает робота по внутренним сторанам квадрата
+function moving_around(r)::Nothing
+    steps_to_marker = [0,0]
 
-    for sides in HorizonSide
-        go_by_border!(r,sides, path)
+    for steps in path[2]
+        move!(r,Nord)
+        steps_to_marker[1] += 1 
+    end
+    putmarker!(r)
+    while (not(isborder(Nord)))
+        move!(r,Nord)
     end
 
-    back_in_start!(r)
+    for steps in path[1]
+        move!(r,Ost)
+        steps_to_marker[2] += 1 
+    end
+    putmarker!(r)
+    while (not(isborder(Ost)))
+        move!(r,Ost)
+    end   
+
+    for steps in steps_to_marker[1]
+        move!(r,Sud)
+    end
+    putmarker!(r)
+    while (not(isborder(Sud)))
+        move!(r,Sud)
+    end 
+
+    for steps in steps_to_marker[2]
+        move!(r,West) 
+    end
+    putmarker!(r)
+    while (not(isborder(West)))
+        move!(r,West)
+    end 
 end
 
-# создание массива path = Int[]
-# добавить элемент в массив push!(path, то что добавляем)
-# reverse(path) или reverse!(path)
-# movements!(r, num::Integer, side::HorizonSide)
-# function f(a::Vector\Matrix\Array{<:Integer})
+function master(r)::Nothing
+    moving_in_angle!(r)
+    moving_around(r)
+    moving_back_to_start(r)
+end
