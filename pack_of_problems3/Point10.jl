9f22d3cc4ca67f0dc156b8ceab3b18e7f0eccfde
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2021-10-01 09:35:51 +0300

    Требуется проверка!

--- pack_of_problems3/Point10.jl
+++ pack_of_problems3/Point10.jl
@@ -1,10 +1,12 @@
-function isMark(r::Robot)::Integer
+# isMark определяет жесть ли в клетке маркер
+function isMark(r::Robot, temperature::Integer, counter::Integer)::Integer
     if (ismarker(r))
-        temperature += temperature(r)
+        temperature += temperature
         counter += 1
     end
 end
 
+# upORdown идёт до упора вверх в вверх или вниз, проверяет наличие маркера на каждом шагу 
 function upORdown(r::Robot,side::HorizonSide)::Nothing
     while not(isborder(r,side))
         move!(r)
@@ -12,22 +14,23 @@ function upORdown(r::Robot,side::HorizonSide)::Nothing
     end
 
 # змейкой проходит все поле и собирает информацию о температуре в клетках
-function (r::Robot)::Integer
-    while not(isborder(r,Ost))
+function tuda_suda(r::Robot)::Integer
         if isborder(r,Sud)
             upORdown(r,((side+2)%4))
-            
         else
             upORdown(r,((side+2)%4))
-        end
-        
-        
-            
+        end     
 end
 
+# master главная функция программа
 function master(r::Robot)::Integer
     temperature = 0
     counter = 0
     isMark(r)
+    while (not(isborder(r,Ost)))
+        tuda_suda(r)
+    end
+    return (temperature/counter)
+end
 
 
