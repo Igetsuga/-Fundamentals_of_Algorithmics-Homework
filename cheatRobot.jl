e9b4b7ddc1e9ccabaa37a29dffc2ef8574ef2afa
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2021-10-18 10:03:19 +0300

    version1

--- cheatRobot.jl
+++ cheatRobot.jl
@@ -5,18 +5,18 @@ end
 function next_side(side::HorizonSide)
     return anti_side(HorizonSide((Int(side) + 1) % 4))
 end
-
+# против часовой
 function next_side_pr(side::HorizonSide)
     return HorizonSide((Int(side) + 1) % 4)
 end
-
+# -----------------------------------------------------------------------------------------------------
 function move_for!(robot::Robot,side::HorizonSide,steps)
     for step in 1:steps
         move!(robot,side)
     end
 end
-
-function try_move!(robot, side)
+# -----------------------------------------------------------------------------------------------------
+function try_move_var1!(robot, side)
     steps = 0
     nextside = next_side(side)
     while isborder(robot, side)
@@ -24,6 +24,9 @@ function try_move!(robot, side)
             move!(robot, nextside)
             steps += 1
         else
+            for j in 1:steps
+                move!(robot, anti_side(nextside))
+            end
             return false
         end
     end
@@ -34,6 +37,41 @@ function try_move!(robot, side)
     end
 
     move_for!(robot, anti_side(nextside), steps)
+    return true
+end
+# -----------------------------------------------------------------------------------------------------
+function try_move_var2!(robot, side)
+    steps = 0
+    nextside = next_side_pr(side)
+    while isborder(robot, side)
+        if !isborder(robot, nextside)
+            move!(robot, nextside)
+            steps += 1
+        else
+            for j in 1:steps
+                move!(robot, anti_side(nextside))
+            end
+            return false
+        end
+    end
+
+    move!(robot, side)
+    while isborder(robot, anti_side(nextside))
+        move!(robot, side)
+    end
+
+    move_for!(robot, anti_side(nextside), steps)
+    return true
+end
+# -----------------------------------------------------------------------------------------------------
+function try_move!(robot,side)
+    try_move_var1!(r,Ost)
+    if (try_move_var1!(r,Ost) == false)
+        try_move_var2!(r,Ost)
+        if (try_move_var2!(robot,side) == false)
+            return false
+        end
+    end
     return true
 end
 
