7503948c86634b75cd0cc5f2899d5c063453f73b
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2021-10-24 00:23:40 +0300

    Done!

--- pack_of_problems1/Point5.jl
+++ pack_of_problems1/Point5.jl
@@ -3,64 +3,47 @@
 
 РЕЗУЛЬТАТ: Робот - в исходном положении и в углах поля стоят маркеры
 """
-flag = true
 
-function anti_side(side::HorizonSide)::HorizonSide
-    return HorizonSide((Int(side) + 2) % 4)
-end
+
+using HorizonSideRobots
+r = Robot(animate=true)
+include("../cheatRobot.jl")
 
 # moving_in_angle! передвинет робота в Юго-Западный угол поля и вернёт в переменную back_bath ОБРАТНЫЙ путь робота
-function moving_in_angle!(robot::Robot, sides)::Vector{<:Integer}
+function moving_in_angle!(robot::Robot, sides)
     back_path = []
-    while (!(isborder(robot, sides[1])) && !(isborder(robot, sides[2])))
-        for side in [sides[1],sides[2]]
+    while (!(isborder(robot, sides[1])) || !(isborder(robot, sides[2])))
+        for side in (sides[1], sides[2])
             steps = 0
-            while (not(isborder(robot,side)))
+            while (!(isborder(robot,side)))
                 move!(robot,side)
                 steps += 1
             end
             push!(back_path, steps)
-        end
-        if (isborder(robot,sides[1] && isborder[robot,sides[2]]))
-            if (side == sides[1])
-                flag = true
-            else
-                flag = false
-            end
         end
     end
     return reverse!(back_path)
 end
 
 # moving_back_to_start! вернёт робота в начальное положение через обратный путь робота
-function moving_back_to_start!(robot::Robot, sides, back_path::Vector{<:Integer})::Nothing
-    if (flag)
-        for (index,value) in enumerate(back_path)
-            for step in value
-                if (index % 2 == 1)
-                    move!(robot, anti_side(sides[1]))
-                else
-                    move!(robot, anti_side(sides[2]))
-                end
+function moving_back_to_start!(robot::Robot,sides,back_path::Vector)
+    for (index,value) in enumerate(back_path)
+        if (index % 2 == 1)
+            for step in 1:value
+                move!(robot, anti_side(sides[2]))
             end
-        end
-    else
-        for (index,value) in enumerate(back_path)
-            for step in value
-                if (index % 2 == 1)
-                    move!(robot, anti_side(sides[2]))
-                else
-                    move!(robot, anti_side(sides[1]))
-                end
-            end
+        else
+            for step in 1:value
+                move!(robot, anti_side(sides[1]))
+            end 
         end
     end
-    flag = true
 end
 
 function Point5_master!(robot::Robot)::Nothing
     for sides in [(West,Sud), (West,Nord), (Ost,Sud), (Ost,Nord)]
         back_path = moving_in_angle!(robot,sides)
+        putmarker!(robot)
         moving_back_to_start!(robot,sides,back_path)
     end
 end
