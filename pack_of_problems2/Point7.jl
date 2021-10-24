10e50754f2ec7630c44c07c3653726c01520e0d8
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2021-10-24 23:11:53 +0300

    проблема с нижним рядом

--- pack_of_problems2/Point7.jl
+++ pack_of_problems2/Point7.jl
@@ -1,67 +1,54 @@
-# ставим маркер в начальной точке робота.
-putmarker!(r)
+```
+ДАНО: Робот - в произвольной клетке ограниченного прямоугольного поля без внутренних перегородок
 
-# path -- путь, который робот проделал из начальной точки в правую нижнюю.
-path = []
-# movements_in_angle! двигает робота из начальной клетки в правую нижнюю.
-function move_in_angle!(r::Robot)::Vector(<:Integer)
-    while (isborder(r,West) and isborder(r,Sud))
-        for side in [Sud,Ost]
-            steps = 0
-            while (not(isborder(r,side)))
-                move!(r,side)
-                steps += 1
-            end
-            push!(path,steps)
-        end 
-        return reverse!(path)
-    end
-end
+РЕЗУЛЬТАТ: Робот - в исходном положении, в клетке с роботом стоит маркер, и все остальные клетки поля промаркированы в шахматном порядке
 
-# глобальная переменная counter, чтобы всем функция она была видна и доступна для изменения.
-counter = 0
+```
 
-# markerovshik! ставит маркер под роботом на каждом четном шагу.
-function markerovshik!(r::Robot, side::HorizonSide, counter::Integer)::Nothing
-    while (not(isborder(r,side)))
-        if (counter%2==0)
-            putmarker!(r)
+using HorizonSideRobots
+r = Robot(animate=false)
+include("../cheatRobot.jl")
+
+function get_one_marked_line!(robot::Robot, side, counter::Integer)
+    while (!(isborder(robot,side)))
+        if (counter % 2 == 1)
+            putmarker!(robot)
+            move!(robot,side)
+        else
+            move!(robot,side)
         end
-        move!(side)
         counter += 1
     end
 end
 
-# Go_right_down! вернет робота в правую нижнюю клетку
-function Go_right_down!(r::Robot)::Nothing
-    for side in [West,Sud]
-        while (not(isborder(r,side)))
-            move!(r)
-        end
-    end
-end
-
-# moving_back_to_start! возвращает робота в начальную точку из правой нижней.
-function moving_back_to_start!(r, path::Vector)::Nothing
-    for i in lenght(path)
-        for a in path[i]
-            move!(r, HorizonSide((i+2)%4))
-        end
-    end
-end
 
 # master! главная функция программы.
-function master!(r::Robot)::Nothing
-    move_in_angle!(r)
-    while (not(isborder(r,Ost)))
+function master!(robot::Robot)::Nothing
+    back_path = moving_in_angle!(robot)
+    counter = 0
+    while ( !(isborder(robot,Ost)) )
         for side in [Nord,Sud]
-            while (not(isborder(r,side)))
-                markerovshik!(r,side,counter)
+            get_one_marked_line!(robot,side,counter)
+            if ( !(isborder(robot, Ost)) )
+                move!(robot, Ost)
+                counter += 1 
+            else
+                continue
             end
-            move!(r,Ost)
-            counter+=1
         end
     end
-    Go_right_down!(r)
-    moving_back_to_start!(r, path)
+
+    if ( isborder(robot, Nord) )
+        for side in [Sud, West]
+            while (!isborder(robot, side))
+                move!(robot, side)
+            end
+        end
+    else
+        while (!isborder(robot, West))
+            move!(robot, West)
+        end
+    end
+        
+    moving_back_to_start!(robot, back_path)
 end 
