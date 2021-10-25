b9c96496e171e2e911a598ffcc0ef9367d54d1fe
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2021-10-25 09:53:53 +0300

    Done!

--- pack_of_problems2/Point9.jl
+++ pack_of_problems2/Point9.jl
@@ -1,27 +1,33 @@
+```
+
+ДАНО: Где-то на неограниченном со всех сторон поле и без внутренних перегородок имеется единственный маркер. 
+Робот - в произвольной клетке поля.
+
+РЕЗУЛЬТАТ: Робот - в клетке с тем маркером.
+
+```
+using HorizonSideRobots
+r = Robot(animate = true)
+include("../cheatRobot.jl")
+
 # будем ходить кругом, пока не найжем этот маркер.
 # 1 Вниз, 2 Вправо, 2 Вверх, 3 Влево, 3 Вниз, 4 Вправо, 4 Вверх и тд.
-function master(r::Robot)::Norhing
-    flag = true
-    counter = 1
+function Point9_master!(r::Robot)
+    flag = false
+    steps = 0
     if (ismarker(r))
-        break
-    else
-        move!(r,Sud)
-        # не знаю правильно ли я использовал картежи
-        for side in [(Ost,Nord),(West, Sud)]
-            for steps in range(counter + 1*(side==Ost or side==Nord))
-                if (not(ismarker(r)))
+        return true
+    else 
+        while !ismarker(r)
+            for side in [Ost, Nord, West, Sud]
+                steps += 1*(side==Ost || side==West)
+                for step in 1:steps
                     move!(r,side)
-                else
-                    flag = false
-                    break
+                    if (ismarker(r))
+                        return true
+                    end
                 end
             end
-            counter += 2
-        end
-        # здесь проверка, на всякий случай, не знают выбросит ли первый break из внешнего if.
-        if (not(flag))
-            break
         end
     end
 end
