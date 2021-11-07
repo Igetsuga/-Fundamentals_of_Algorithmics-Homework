2c4f930ca99f21bcf4fa29585725da8ac9f65fcf
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2021-11-07 15:23:43 +0300

    добавлена функция moving_in_angle!

--- use.jl
+++ use.jl
@@ -109,3 +109,30 @@ function try_move!(robot, side)::Bool
         return true
     end
 end
+
+
+
+# moving_in_angle! передвинет робота в Юго-Западный угол поля и вернёт в переменную back_bath ОБРАТНЫЙ путь робота
+function moving_in_angle!(robot::Robot)
+    back_path = []
+    while (!(isborder(robot, West)) || !(isborder(robot, Sud)))
+        for side in (Sud, West)
+            steps = 0
+            while (!(isborder(robot,side)))
+                move!(robot,side)
+                steps += 1
+            end
+            push!(back_path, steps)
+        end
+    end
+    return reverse!(back_path)
+end
+
+# moving_back_to_start! вернёт робота в начальное положение через обратный путь робота
+function moving_back_to_start!(robot::Robot, back_path::Vector)
+    for (index,value) in enumerate(back_path)
+        for step in 1:value
+            move!(robot, HorizonSide((((index + 1)% 2) + 3) % 4))
+        end
+    end
+end
