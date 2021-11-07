9dd2b1e638b7862aeeeb78e95d717063bb11b17c
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2021-11-07 13:56:59 +0300

    готовая функция try_move

--- use.jl
+++ use.jl
@@ -38,51 +38,61 @@ function move_steps!(robot,side::HorizonSide,steps)
     end
 end
 # -----------------------------------------------------------------------------------------------------
-function try_move_var2!(robot, side)
+function try_move_var2!(robot, side)::Bool
     steps = 0
     nextside = next_side_pr(side)
+    # робот пытается обойти перегодку со стороны противоположной часовой стрелке
     while isborder(robot, side)
         if !isborder(robot, nextside)
             move!(robot, nextside)
             steps += 1
         else
+            # если обойти невозможно вернется на исходное место
             for j in 1:steps
                 move!(robot, anti_side(nextside))
             end
+            # обойти прегородку невозможно
             return false
         end
     end
 
+    # обход перегородки в ширину
     move!(robot, side)
     while isborder(robot, anti_side(nextside))
         move!(robot, side)
     end
 
     move_steps!(robot, anti_side(nextside), steps)
-    move!(r,side)
+    move!(robot,side)
     return true
 end
 # -----------------------------------------------------------------------------------------------------
-function try_move!(robot, side)
-    if isborder(r,side)
+function try_move!(robot, side)::Bool
+
+    # если перед роботом есть перегородка, он начнет выполнять действия, чтобы попытаться её обойти, иначе просто пройдет в заданном направлении
+    if isborder(robot,side)
         flag_nextside = true
         steps = 0
         nextside = next_side(side)
+        # сначала робот пытается обойти перегородку со строны, которая следует за заданной по часовой стрелке
         while isborder(robot, side)
             if !isborder(robot, nextside)
                 move!(robot, nextside)
                 steps += 1
             else
-                
+                # срабатывает, когда робот уперся в угол. Он возвращается в то место, с которого начал попытку обойти по часовой стороне
                 for j in 1:steps
                     move!(robot, anti_side(nextside))
                 end
 
                 flag_nextside = false
 
-                return try_move_var2!(r,side)
+                # робот попытается обойти перегородку со сторны противоположной часовой стрелке
+                return try_move_var2!(robot,side)
             end
         end
+
+        # если у робота получилось обойти перегородку с первого раза, будет выполнено слудущее:
         if (flag_nextside) 
             move!(robot, side)
             while isborder(robot, anti_side(nextside))
@@ -93,8 +103,9 @@ function try_move!(robot, side)
             return true
         end
     else
-        move!(r,side)
 
+        # перегородки не было, робот прошел в заданном направлении
+        move!(robot,side)
         return true
     end
 end
