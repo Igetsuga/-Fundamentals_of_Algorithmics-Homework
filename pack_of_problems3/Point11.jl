d4e8ba16afcef5c7026801e553a7f46da3055f73
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2021-11-14 15:37:59 +0300

    нужный робот пока не готов

--- pack_of_problems3/Point11.jl
+++ pack_of_problems3/Point11.jl
@@ -14,19 +14,18 @@ include("../use.jl")
 
 function move_to_border!(robot::Robot, side::HorizonSide)
     steps = 0
-    while (try_move!(r,side))
+    while (try_move!(robot,side))
         steps += 1
-        push!(Steps,steps)
-        steps = 0
-        push!(Sides,Int(side))
     end
-    
+    push!(Steps,steps)
+    steps = 0
+    push!(Sides,Int(side))
 end
 
 function master!(robot::Robot)::Nothing
     for side in [Nord, Sud, West, Ost]
         move_to_border!(robot,side)
-        putmarker!(r)
-        move_back!(r,reverse!(Steps),reverse!(Sides))
+        putmarker!(robot)
+        move_back!(robot,reverse!(Steps),reverse!(Sides))
     end
 end
