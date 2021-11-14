b389ffed5d365131d13c251b7e6eae38180679b1
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2021-11-14 17:41:31 +0300

    Update AbstractType.jl

--- AbstractType.jl
+++ AbstractType.jl
@@ -62,4 +62,70 @@ get_coords(robot::CoordsRobot) = (robot.x , robot.y)
 
 #------------------------------------------------------------------------------------------------------------------------------#
 
+mutable struct Beta <: AbstractRobot
+    beta::Robot
+    path::Vector
+end
+
+beta = Beta(originRobot, [])
+
+get(robot::Beta) = robot.beta
+
+
+function move!(robot::Beta, side::HorizonSide)
+    push!(path, side)
+    move!(robot.beta, side)
+end
+
+function move_back!(robot::Beta, path::Vector)::Nothing
+    for i in 1:length(path)
+        move!(robot.beta, anti_side(HorizonSide(path[ length(path) - i + 1 ])))
+    end
+    empty!(Steps)
+    empty!(Sides)
+end
+
+function try_move!(robot::Beta, side::HorizonSide)::Bool
+
+    # если перед роботом есть перегородка, он начнет выполнять действия, чтобы попытаться её обойти, иначе просто пройдет в заданном направлении
+    if isborder(robot, side)
+        flag_nextside = true
+        steps = 0
+        nextside = next_side(side)
+        # робот пытается обойти перегородку со строны, которая следует за заданной по часовой стрелке
+        while isborder(robot, side)
+            if !isborder(robot, nextside)
+                move!(robot, nextside)
+                steps += 1
+            else
+                # срабатывает, когда робот уперся в угол. Он возвращается в то место, с которого начал попытку обойти по часовой стороне
+                for j in 1:steps
+                    move!(robot, anti_side(nextside))
+                end
+
+                flag_nextside = false
+
+                return false
+            end
+        end
+
+        # если у робота получилось обойти перегородку с первого раза, будет выполнено слудущее:
+        if (flag_nextside) 
+            move!(robot, side)
+            while isborder(robot, anti_side(nextside))
+                move!(robot, side)
+            end
+            move_steps!(robot, anti_side(nextside), steps)
+
+            return true
+        end
+    else
+        # перегородки не было, робот прошел в заданном направлении
+        move!(robot,side)
+        return true
+    end
+end
+    
+
+
 
