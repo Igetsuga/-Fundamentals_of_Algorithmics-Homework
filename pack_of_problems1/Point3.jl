4b0aea21c02b3df53f7dbb526b9670d206fa54e2
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2021-11-15 11:57:19 +0300

    Done!

--- pack_of_problems1/Point3.jl
+++ pack_of_problems1/Point3.jl
@@ -4,14 +4,14 @@
 РЕЗУЛЬТАТ: Робот - в исходном положении, и все клетки поля промакированы
 """
 
-using HorizonSideRobots
+
 # moving_in_angle! передвинет робота в Юго-Западный угол поля и вернёт в переменную back_bath ОБРАТНЫЙ путь робота
 function moving_in_angle!(robot::Robot)
     back_path = []
-    while (!(isborder(robot, West)) && !(isborder(robot, Sud)))
-        for side in [Sud, Ost]
+    while (!(isborder(robot, West)) || !(isborder(robot, Sud)))
+        for side in (Sud, West)
             steps = 0
-            while (not(isborder(robot,side)))
+            while (!(isborder(robot,side)))
                 move!(robot,side)
                 steps += 1
             end
@@ -20,25 +20,26 @@ function moving_in_angle!(robot::Robot)
     end
     return reverse!(back_path)
 end
+
 # moving_back_to_start! вернёт робота в начальное положение через обратный путь робота
-function moving_back_to_start!(robot::Robot, back_path::Vector)
+function moving_back_to_start!(robot::Robot, back_path::Vector)::Nothing
     for (index,value) in enumerate(back_path)
-        for step in value
-            move!(robot, HorizonSide((index + 1) % 2))
+        for step in 1:value
+            move!(robot, HorizonSide((((index + 1)% 2) + 3) % 4))
         end
     end
 end
 # get_lines промаркирует всё поле змейкой
 function get_lines!(robot::Robot)
-    while (!(isborder(robot,West)))
+    while (!(isborder(robot,Ost)))
         for side in [Nord,Sud]
             putmarker!(robot)
             while (!(isborder(robot,side)))
                 move!(robot, side)
                 putmarker!(robot)
             end
-            if (!(isborder(robot,West)))
-                move!(robot,West)
+            if (!(isborder(robot,Ost)))
+                move!(robot,Ost)
             end
         end
     end
@@ -48,6 +49,7 @@ end
 function Point3_master!(robot::Robot)
     # moving_in_angle! передвинет робота в Юго-Западный угол поля и вернёт в переменную back_bath ОБРАТНЫЙ путь робота
     back_path = moving_in_angle!(robot)
+    
     putmarker!(robot)
     # get_lines промаркирует всё поле змейкой
     get_lines!(robot)
