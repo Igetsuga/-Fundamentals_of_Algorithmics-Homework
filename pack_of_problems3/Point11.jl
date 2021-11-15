1a298c01fdcf18c5a4e7486f3914ca64869b95ba
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2021-11-15 09:47:11 +0300

    Done!

--- pack_of_problems3/Point11.jl
+++ pack_of_problems3/Point11.jl
@@ -9,23 +9,16 @@
 ```
 
 using HorizonSideRobots
-include("../use.jl")
+originRobot = Robot(animate = true)
+include("AbstractType.jl")
+include("functions.jl")
 
 
-function move_to_border!(robot::Robot, side::HorizonSide)
-    steps = 0
-    while (try_move!(robot,side))
-        steps += 1
-    end
-    push!(Steps,steps)
-    steps = 0
-    push!(Sides,Int(side))
-end
 
-function master!(robot::Robot)::Nothing
-    for side in [Nord, Sud, West, Ost]
-        move_to_border!(robot,side)
-        putmarker!(robot)
-        move_back!(robot,reverse!(Steps),reverse!(Sides))
+function master(robot::Beta)
+    for side in [Nord, Ost, Sud, West]
+        while (try_move!(robot, side)) end
+        putmarker!(robot.beta)
+        get_back!(robot)
     end
 end
