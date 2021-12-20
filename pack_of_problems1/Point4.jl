162c7e6d6e4ae8f4e505af3999927752b21c3316
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2021-12-20 10:19:50 +0300

    Done!

--- pack_of_problems1/Point4.jl
+++ pack_of_problems1/Point4.jl
@@ -6,110 +6,69 @@
 """
 
 using HorizonSideRobots
-originRobot = Robot(animate = true)
+originRobot = Robot(5,7,animate = false)
 include("../functions.jl")
 
+
 # get_back_smpl возвращает робота к стене обратно заданному направлению, если на пути нет перегородок
-get_back_smpl!(robot::Robot,side::HorizonSide)::Nothing
+function get_back_smpl!(robot::Robot, side::HorizonSide)::Nothing
     while (!(isborder(robot,side)))
         move!(robot,side)
     end
 end
 
-# moving_in_angle! передвинет робота в Юго-Западный угол поля и вернёт в переменную back_bath ОБРАТНЫЙ путь робота
-#=
-function moving_in_angle!(robot::Robot)::Vector{<:Integer}
-    back_path = []
-    while (not(isborder(robot, West)) and not(isborder(robot, Sud)))
-        for side in [Sud, Ost]
-            steps = 0
-            while (not(isborder(robot,side)))
-                move!(robot,side)
-                steps += 1
-            end
-            push!(back_path, steps)
-        end
-    end
-    return reverse!(back_path)
-end
-# moving_back_to_start! вернёт робота в начальное положение через обратный путь робота
-function moving_back_to_start!(robot::Robot, back_path::Vector{<:Integer})::Nothing
-    for (index,value) in enumerate(back_path)
-        for step in value
-            move!(robot, HorizonSide((index + 1) % 2))
-        end
-    end
-end
-=#
+function liner!(robot, moveSide, upSide)
 
-# get_horizontal_lines промаркирует необходимые клетки
-function get_horizontal_lines!(robot::Robot)::Nothing
     steps = 0
-    putmarker!(robot)
-    while (not(isborder(robot, West)))
-        move!(robot,West)
-        putmarker!(robot)
-        steps += 1
-    end
-    get_back_smpl!(robot,side)
-    steps -= 1
+    isBorder = false
 
-    while (not(isborder(robot,Nord)) and steps > 1)
-        move!(r,Nord)
+    while !isborder(robot, moveSide)
+        move!(robot, moveSide)
+        steps += 1
         putmarker!(robot)
-        for step in steps 
-            move!(robot, West)
+    end 
+    get_back_smpl!(robot, anti_side(moveSide))
+    
+    if !isborder(robot, upSide)
+        move!(robot, upSide)
+    else
+        isBorder = true
+    end
+
+    while !isBorder && steps > 0
+        steps -= 1
+        putmarker!(robot)
+        for _ in 1:steps
+            move!(robot, moveSide)
             putmarker!(robot)
         end
-        steps -= 1
+
+        get_back_smpl!(robot, anti_side(moveSide))
+
+        if !isborder(robot, upSide)
+            move!(robot, upSide)
+        else
+            isBorder = true
+            break
+        end
     end
 end
 
-# Point4_master! 
-function master4!(r::Robot)::Nothing
-    back_bath = moving_in_angle!(robot)
+# master4! 
+function master4!(robot::Robot)::Nothing
+    back_path = moving_in_angle!(robot)
     putmarker!(robot)
     
-    get_horizontal_lines!(robot)
-    
-    for side in [Sud,West]
-        while (not(isboerder(robot,side)))
-            move!(robot,side)
-        end
-    end
-    
+    liner!(robot, Ost, Nord)
+
+    moving_in_angle!(robot)
     # moving_back_to_start! вернёт робота в начальное положение через обратный путь робота
     moving_back_to_start!(robot, back_path)
 end
 
-steps = 0
-while !isborder(robot, moveSide)
-    move!(robot, moveSide)
-    steps += 1
-    putmarker!(robot)
-end
-get_back_smpl!(robot, anti_side(moveSide))
-if !isborder(robot, upSide)
-    move!(robot, upSide)
-else
-    break
-end
-isborder = false
-while !isborder && steps > 0
-    steps -= 1
-    putmarker!(robot)
-    for _ in 1:steps
-        move!(robot, moveSide)
-        putmarker!(robot)
-    end
-    get_back_smpl!(robot, anti_side(moveSide))
-    if !isborder(robot, upSide)
-        move!(robot, upSide)
-    else
-        isborder = true
-        break
-    end
-end
+master4!(originRobot)
+show!(originRobot)
+
 
 
     
