7156f14757b70a8eab21694e61e587799c1197da
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2021-12-20 08:36:39 +0300

    Done!

--- pack_of_problems1/Point5.jl
+++ pack_of_problems1/Point5.jl
@@ -1,13 +1,13 @@
 """
-ДАНО: Робот - в произвольной клетке ограниченного прямоугольного поля, на котором могут находиться также внутренние прямоугольные перегородки (все перегородки изолированы друг от друга, прямоугольники могут вырождаться в отрезки)
+ДАНО: Робот - в произвольной клетке ограниченного прямоугольного поля, на котором могут находиться также
+внутренние прямоугольные перегородки (все перегородки изолированы друг от друга, прямоугольники могут вырождаться в отрезки)
 
 РЕЗУЛЬТАТ: Робот - в исходном положении и в углах поля стоят маркеры
 """
 
 
 using HorizonSideRobots
-r = Robot(animate=true)
-include("../cheatRobot.jl")
+originRobot = Robot(animate=true, "5example.sit")
 
 # moving_in_angle! передвинет робота в Юго-Западный угол поля и вернёт в переменную back_bath ОБРАТНЫЙ путь робота
 function moving_in_angle!(robot::Robot, sides)
@@ -40,10 +40,12 @@ function moving_back_to_start!(robot::Robot,sides,back_path::Vector)
     end
 end
 
-function Point5_master!(robot::Robot)::Nothing
+function master5!(robot::Robot)::Nothing
     for sides in [(West,Sud), (West,Nord), (Ost,Sud), (Ost,Nord)]
         back_path = moving_in_angle!(robot,sides)
         putmarker!(robot)
         moving_back_to_start!(robot,sides,back_path)
     end
 end
+
+master5!(originRobot)
